4749bee612812e895a0db29ef6f8fb7a8da0dbea
lihongxiang97 <74760197+lihongxiang97@users.noreply.github.com>
2024-09-03 17:17:29 +0800

    Create get_sd.py

---
+++ 4. Functional divergence of duplications/Scripts/get_sd.py
@@ -0,0 +1,15 @@
+import sys
+import pandas as pd
+if len(sys.argv) != 4:
+  print('Usage: python get_sd.py gene_pairs sd_file type')
+else:
+  gene_pair = []
+  with open(sys.argv[1]) as f:
+    for lines in f:
+      line = lines.strip().split()
+      gene_pair.append(line)
+  sd_matrix = pd.read_parquet(sys.argv[2])
+  for i in gene_pair:
+    if i[0] in sd_matrix.index and i[1] in sd_matrix.index:
+      sd = sd_matrix[i[0]][i[1]]
+      print(sys.argv[3],sd,sep='\t')
